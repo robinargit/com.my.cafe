2ca0e32b8f3c11e97e8c4fbf7e03c6088f604e03
robinargit <robinargit@gmail.com>
2023-01-31 21:10:52 +0600

    commit done after log4j2 implemention

--- src/main/java/com/my/cafe/com/Application.java
+++ src/main/java/com/my/cafe/com/Application.java
@@ -2,12 +2,29 @@ package com.my.cafe.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+	   // SpringApplication.run(Application.class, args);
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
+
+
+        LOGGER.info("Spring Boot Application Started");
+
+
 	}
 
 }
--- src/main/java/com/my/cafe/com/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/my/cafe/com/serviceImpl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.my.cafe.com.serviceImpl;
 
+import com.my.cafe.com.Application;
 import com.my.cafe.com.POJO.User;
 import com.my.cafe.com.contests.CafeConstant;
 import com.my.cafe.com.dao.UserDao;
 import com.my.cafe.com.service.UserService;
 import com.my.cafe.com.utils.CafeUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +17,20 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.Objects;
 
-@Slf4j
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserDao userDao;
 
+    private static final Logger LOGGER = LogManager.getLogger(UserServiceImpl.class);
+
 
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+
+        LOGGER.info("Request comes here");
 
         try {
 
@@ -31,9 +38,13 @@ public class UserServiceImpl implements UserService {
 
                 User user = userDao.findByEmaiid(requestMap.get("email"));
 
+                LOGGER.info("Hello from Log4j 2 - user email"+ user);
+
                 if (Objects.isNull(user)) {
 
                     userDao.save(getUserFromMap(requestMap));
+
+                    LOGGER.info("data inserted with "+ requestMap);
 
                 } else {
 
